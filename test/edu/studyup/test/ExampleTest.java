a0352124018509f4f7b4e6cbd5c674367069997f
vhellendoorn <vhellendoorn@live.nl>
2019-01-25 11:36:56 -0800

    Add testing scaffolding

---
+++ test/edu/studyup/test/ExampleTest.java
@@ -0,0 +1,42 @@
+package edu.studyup.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("JUnit 5 Example")
+class ExampleTest {
+ 
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all test methods");
+    }
+ 
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each test method");
+    }
+ 
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each test method");
+    }
+ 
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all test methods");
+    }
+    
+    @Test
+    public void test1() {
+    	System.out.println("Ran test1");
+    }
+    
+    @Test
+    public void test2() {
+    	System.out.println("Ran test2");
+    }
+}
